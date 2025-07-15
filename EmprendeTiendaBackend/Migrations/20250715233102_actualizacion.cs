8ac7e9f01b74f3cdb961d6cdd96277046f0afc80
LuisinaBender <luisinabender@hotmail.com>
2025-07-15 20:31:32 -0300

    Eliminar entidad DetalleVenta y actualizar migraciones
    Se eliminó la entidad `DetalleVenta` del modelo de datos, junto con sus referencias en `AppDbContextModelSnapshot.cs`.
    Se actualizó la fecha de las ventas en la tabla `Ventas` a un valor predeterminado.
    Se creó una nueva migración llamada `actualizacion` que refleja estos cambios y se ajustó el modelo en `actualizacion.Designer.cs`.

---
+++ EmprendeTiendaBackend/Migrations/20250715233102_actualizacion.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmprendeTiendaBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class actualizacion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetalleVenta");
+
+            migrationBuilder.UpdateData(
+                table: "Ventas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Fecha",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Ventas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Fecha",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Ventas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Fecha",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DetalleVenta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProductoId = table.Column<int>(type: "int", nullable: false),
+                    VentaId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    PrecioUnitario = table.Column<decimal>(type: "decimal(65,30)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetalleVenta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetalleVenta_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetalleVenta_Ventas_VentaId",
+                        column: x => x.VentaId,
+                        principalTable: "Ventas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Ventas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Fecha",
+                value: new DateTime(2025, 7, 10, 17, 22, 20, 70, DateTimeKind.Local).AddTicks(2897));
+
+            migrationBuilder.UpdateData(
+                table: "Ventas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Fecha",
+                value: new DateTime(2025, 7, 12, 17, 22, 20, 70, DateTimeKind.Local).AddTicks(2913));
+
+            migrationBuilder.UpdateData(
+                table: "Ventas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Fecha",
+                value: new DateTime(2025, 7, 14, 17, 22, 20, 70, DateTimeKind.Local).AddTicks(2914));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleVenta_ProductoId",
+                table: "DetalleVenta",
+                column: "ProductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleVenta_VentaId",
+                table: "DetalleVenta",
+                column: "VentaId");
+        }
+    }
+}
