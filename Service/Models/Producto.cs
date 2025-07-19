f825e2e03152125e66cdb9ea57342a35b269bdc7
LuisinaBender <luisinabender@hotmail.com>
2025-07-18 22:34:21 -0300

    Añadir inicialización a DetalleVentas en Producto
    Se ha modificado la propiedad `DetalleVentas` en la clase `Producto` para que se inicialice como una nueva lista de `DetalleVenta` al momento de crear una instancia. Esto previene que `DetalleVentas` sea nulo y permite su uso inmediato sin necesidad de inicialización adicional en el constructor.

--- Service/Models/Producto.cs
+++ Service/Models/Producto.cs
@@ -5,14 +5,12 @@ namespace Service.Models
     public class Producto
     {
         public int Id { get; set; }
-
-
-        [Required(ErrorMessage = "El campo Nombre es Obligatorio")] public string Nombre { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es Obligatorio")]
+        public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public decimal Precio { get; set; }
         public int Stock { get; set; }
-
-        public ICollection<DetalleVenta> DetalleVentas { get; set; }
+        public ICollection<DetalleVenta> DetalleVentas { get; set; } = new List<DetalleVenta>();
     }
 }
 
